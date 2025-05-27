e44321b71550ad0bf16a6989d790dbc1fee3ef0b
AFSilver <felipe.correa@beltcom.co>
2025-05-27 11:48:25 -0500

    Big Redesign
    The page ain't ugly anymore

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,43 +21,28 @@ document.addEventListener('DOMContentLoaded', () => {
 			})
 		}
 	}
-
-	var my_nav = $('.navbar-sticky'); 
-	//grab the initial top offset of the navigation 
-	var sticky_navigation_offset_top = my_nav.offset().top;
-	
-	//function that decides weather the navigation bar should have "fixed" css position or not.
-	var sticky_navigation = function(){
-		var scroll_top = $(window).scrollTop(); // our current vertical position from the top
-		
-		//if we've scrolled more than the navigation, change its position to fixed to stick to top, otherwise change it back to relative
-		if (scroll_top > sticky_navigation_offset_top) { 
-			my_nav.addClass( 'stick' );
-		} else {
-			my_nav.removeClass( 'stick' );
-		}   
-	};
-
-	var initio_parallax_animation = function() { 
-		$('.parallax').each( function(i, obj) {
-			var speed = $(this).attr('parallax-speed');
-			if( speed ) {
-				var background_pos = '-' + (window.pageYOffset / speed) + "px";
-				$(this).css( 'background-position', 'center ' + background_pos );
-			}
-		});
-	}
-	
-	//run function on load
-	sticky_navigation();
-	
-	//and run it again every time user scrolls
-	$(window).scroll(function() {
-		 sticky_navigation();
-		 initio_parallax_animation();
-	});
-
 });
+
+//typewriter animation
+const observer = new IntersectionObserver((entries, observer) => {
+	entries.forEach(entry => {
+	if (entry.isIntersecting) {
+		startTyping();
+		observer.unobserve(entry.target); // Only trigger once
+	}
+	});
+});
+
+function startTyping(){
+	const typed = document.querySelector('typed-text');
+  	const text = typed.textContent;
+ 	const steps = text.length;
+	typed.style.width = '0ch';
+  	typed.style.animation = `typing 2s steps(${steps}, end) forwards`;
+}
+
+
+
 
 //For projects detailing more info on-click
 function openPopupESP(popupId) {
